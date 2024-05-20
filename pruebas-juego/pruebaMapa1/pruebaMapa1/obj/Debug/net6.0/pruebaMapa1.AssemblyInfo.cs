ae43ee80b154bf713f863f769f7e7d86ecb8be91
Muriel <soiffermuriel@gmail.com>
2024-05-20 00:45:35 -0300

    prueba 2 de mapa un exito solo faltan las colisiones

--- pruebas-juego/pruebaMapa1/pruebaMapa1/obj/Debug/net6.0/pruebaMapa1.AssemblyInfo.cs
+++ pruebas-juego/pruebaMapa1/pruebaMapa1/obj/Debug/net6.0/pruebaMapa1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pruebaMapa1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83e9eebe74c69774096b686f6031cb00601ff067")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f5b10fec5a602211706f0e130a19dfe20c62813")]
 [assembly: System.Reflection.AssemblyProductAttribute("pruebaMapa1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pruebaMapa1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
