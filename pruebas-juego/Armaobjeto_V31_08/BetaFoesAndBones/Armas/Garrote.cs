1baee87e664f0ba3d162beffd5c8ee9c818d51a3
Soria64capo <soriarodrigomartin@gmail.com>
2024-09-02 19:39:52 -0300

    Garrote vacio

---
+++ pruebas-juego/Armaobjeto_V31_08/BetaFoesAndBones/Armas/Garrote.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpriteBatch = Microsoft.Xna.Framework.Graphics.SpriteBatch;
+
+namespace BetaFoesAndBones.Armas
+{
+    internal class Garrote : Componentes
+    {
+
+        public Garrote(Game1 game, ContentManager contenedor)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch sprite)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
