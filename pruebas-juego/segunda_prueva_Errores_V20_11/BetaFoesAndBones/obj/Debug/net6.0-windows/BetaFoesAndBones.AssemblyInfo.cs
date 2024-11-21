003e51d247160bb786a3fed0e3731143c49d6218
gino mazzaglia <mazzagliagino@gmail.com>
2024-11-20 22:48:33 -0300

    pongo el jefe en la habitacion final

--- pruebas-juego/segunda_prueva_Errores_V20_11/BetaFoesAndBones/obj/Debug/net6.0-windows/BetaFoesAndBones.AssemblyInfo.cs
+++ pruebas-juego/segunda_prueva_Errores_V20_11/BetaFoesAndBones/obj/Debug/net6.0-windows/BetaFoesAndBones.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BetaFoesAndBones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7534b544b4d0ec4e3fc24cc922819900a278cd76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe76e0e92b741df6c4eea10bd0b6c449b43695c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BetaFoesAndBones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BetaFoesAndBones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
